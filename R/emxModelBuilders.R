016690e689df447526673bbd8b6a243d3e1c0e00
mhunter <mhunter@ou.edu>
2015-10-29 09:16:01 -0500

    Add the correct ML regression model from Joshua Pritikin

--- R/emxModelBuilders.R
+++ R/emxModelBuilders.R
@@ -149,7 +149,7 @@ emxGrowthModel <- function(model, data, name, run=FALSE, identification, use, or
 #------------------------------------------------------------------------------
 # Regression with FIML
 
-emxRegressionModel <- function(model, data, run, ...){
+emxRegressionModel <- function(model, data, type='Steven', run, ...){
 	theFormu <- model #a formula as passed to lm
 	theFrame <- lm(theFormu, data=data, na.action=na.pass, method='model.frame', ...)
 	theTerms <- attr(theFrame, 'terms')
@@ -167,14 +167,24 @@ emxRegressionModel <- function(model, data, run, ...){
 	namAll <- c(namRespo, namTerms)
 	cdat <- cbind(theRespo, theMatri)
 	colnames(cdat) <- namAll
-	regPaths <- OpenMx::mxPath(from=namTerms, to=namRespo, free=TRUE, values=.8, labels=paste(namRespo, '_on_', namTerms, sep=''))
-	predVars <- OpenMx::mxPath(from=namTerms, arrows=2, free=TRUE, values=1, labels=paste('var', namTerms, sep='_'))
-	residVar <- OpenMx::mxPath(from=namRespo, arrows=2, free=TRUE, values=1, labels=paste('residVar', namRespo, sep='_'))
-	theInter <- OpenMx::mxPath(from='one', to=namRespo, labels='Intercept', values=0, free=!is.na(whichInt))
-	theMeans <- OpenMx::mxPath(from='one', to=namTerms, labels=paste('mean', namTerms, sep='_'))
+	
 	theData <- OpenMx::mxData(cdat, 'raw')
-
-	model <- OpenMx::mxModel('model', type='RAM', manifestVars=namAll, theInter, regPaths, residVar, theMeans, predVars, theData)
+	theInter <- OpenMx::mxPath(from='one', to=namRespo, labels='Intercept', values=0, free=!is.na(whichInt))
+	residVar <- OpenMx::mxPath(from=namRespo, arrows=2, free=TRUE, values=1, labels=paste('residVar', namRespo, sep='_'))
+	
+	if (type=='Joshua'){
+		namLat <- paste0(namTerms, "Latent")
+		structPaths <- OpenMx::mxPath(from='one', to=namLat, free=FALSE, labels=paste0('data.', namTerms))
+		regPaths <- OpenMx::mxPath(from=namLat, to=namRespo, labels=paste0(namRespo, '_on_', namTerms))
+		model <- OpenMx::mxModel('model', type='RAM', manifestVars=namRespo, latentVars=namLat, theData, theInter, residVar, structPaths, regPaths)
+	} else if(type=='Steven'){
+		regPaths <- OpenMx::mxPath(from=namTerms, to=namRespo, free=TRUE, values=.8, labels=paste(namRespo, '_on_', namTerms, sep=''))
+		predVars <- OpenMx::mxPath(from=namTerms, arrows=2, free=TRUE, values=1, labels=paste('var', namTerms, sep='_'))
+		theMeans <- OpenMx::mxPath(from='one', to=namTerms, labels=paste('mean', namTerms, sep='_'))
+		model <- OpenMx::mxModel('model', type='RAM', manifestVars=namAll, theInter, regPaths, residVar, theMeans, predVars, theData)
+	} else {
+		stop(paste('Unknown type argument, ', type, ', in emxRegressionModel', sep=''))
+	}
 	
 	if(run==TRUE){
 		model <- OpenMx::mxRun(model)
