83328a6799712725066877349822b668ba1f2a6f
mhunter <mhunter@ou.edu>
2017-04-29 15:53:12 -0500

    Stop using OpenMx:::single.na

--- R/emxMixture.R
+++ R/emxMixture.R
@@ -14,7 +14,7 @@ emxMixtureModel <- function(model, data, run=FALSE, p=NA, ...){
 		models[[i]]$data <- NULL
 	}
 	model <- OpenMx::mxModel(model='MixtureModel', models, data)
-	if(OpenMx:::single.na(p)){
+	if(length(p)==1 && is.na(p)){
 		p1 <- OpenMx::mxMatrix('Full', nrow=length(models), ncol=1, values=1, lbound=1e-6, free=c(FALSE, rep(TRUE, length(models)-1)), name='theSmallPMatrix', labels=paste0('unscaledProportionForModel', modelNames))
 		theSmallPMatrix <- NULL
 		p2 <- OpenMx::mxAlgebra((1/sum(theSmallPMatrix)) %x% theSmallPMatrix, name='theScaledPMatrix')
